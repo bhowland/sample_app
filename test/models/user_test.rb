44c641e952f3ee7691d65287da125a09d267379f
bhowland <bhowland@madisoncollege.edu>
2015-04-07 19:55:37 +0000

    finish log in/ log out

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -61,4 +61,7 @@ test "email validation should accept valid addresses" do
     assert_not @user.valid?
   end
 
+  test "authenticated? should return false for a user with nil digest" do
+    assert_not @user.authenticated?('')
+  end
 end
